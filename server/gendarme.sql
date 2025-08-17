84f369de1abcbce1dca1d9141231250399799cf9
Adrianna1135 <adriannaharinoro@gmail.com>
2025-08-17 17:06:54 +0300

    Ajouter fichier sql

---
+++ server/gendarme.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  Dim 17 août 2025 à 16:05
+-- Version du serveur :  5.7.17
+-- Version de PHP :  5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `gendarme`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `agents`
+--
+
+CREATE TABLE `agents` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `prenom` varchar(50) DEFAULT NULL,
+  `contact` varchar(255) DEFAULT NULL,
+  `grade` varchar(50) DEFAULT NULL,
+  `service` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `agents`
+--
+
+INSERT INTO `agents` (`id`, `nom`, `prenom`, `contact`, `grade`, `service`) VALUES
+(1, 'Andriamaharavo', 'Adrien', '0341348493', 'GDC', 'IDH');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `agents`
+--
+ALTER TABLE `agents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `agents`
+--
+ALTER TABLE `agents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
